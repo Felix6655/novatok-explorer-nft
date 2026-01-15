be9ea3ead1abef0698c94532e7a5a59f5034952e
Jose L Leon <YOUR_EMAIL_HERE>
2026-01-15 05:35:44 -0800

    trigger redeploy

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-ethers");
 require("@nomicfoundation/hardhat-verify");
 
@@ -12,14 +13,17 @@ module.exports = {
       },
     },
   },
+
   networks: {
     hardhat: {},
+
     sepolia: {
       url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
-      accounts: process.env.DEPLOYER_PRIVATE_KEY ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111,
     },
   },
+
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
